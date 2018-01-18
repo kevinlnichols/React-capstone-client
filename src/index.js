c061cab1c8fbe82e86ab3cef84fe17611f4af3b9
Kevin Nichols <kevinlnichols@gmail.com>
2018-01-18 15:23:14 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import './index.css';
+import App from './components/app';
+import registerServiceWorker from './registerServiceWorker';
+import store from './store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
