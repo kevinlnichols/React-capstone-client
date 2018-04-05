3fc1f1d5920c9f86e9e8c9cdd4e6b7aafd6d6734
Kevin Nichols <kevinlnichols@gmail.com>
2018-04-04 21:54:57 -0400

    update

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-export const API_BASE_URL =
-process.env.REACT_APP_API_BASE_URL || 'https://intense-journey-94565.herokuapp.com/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://intense-journey-94565.herokuapp.com/api';
